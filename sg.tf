9ac15223dc969b603a058615364e00f1c0159d0e
lexaa666 <lexusbirulya@gmail.com>
2023-09-10 21:17:47 +0300

    add new port for project

--- sg.tf
+++ sg.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "allow_http_ssh_pub" {
   description = "Allow http and ssh  pub inbound traffic"
   vpc_id = aws_vpc.ab_diplom_vpc.id
   ingress {
-    description = "port_for_Apache_Superset"
+    description = "cadvisor"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -13,6 +13,20 @@ resource "aws_security_group" "allow_http_ssh_pub" {
     description = "http"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    description = "superset_app"
+    from_port   = 8088
+    to_port     = 8088
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
